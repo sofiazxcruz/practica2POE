6164c3fcbdf3907512740fe04dc155065015724f
sofiazxcruz <127714398+sofiazxcruz@users.noreply.github.com>
2023-08-18 20:55:41 -0600

    Add files via upload

---
+++ practica2POE/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace practica2POE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.atras = new System.Windows.Forms.Button();
+            this.adelante = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Antes del evento";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(43, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Antes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(180, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 56);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Despues ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(492, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 131);
+            this.panel1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(550, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 51);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Cambiar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // atras
+            // 
+            this.atras.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.atras.Location = new System.Drawing.Point(43, 422);
+            this.atras.Name = "atras";
+            this.atras.Size = new System.Drawing.Size(105, 44);
+            this.atras.TabIndex = 6;
+            this.atras.Text = "<<";
+            this.atras.UseVisualStyleBackColor = true;
+            this.atras.Click += new System.EventHandler(this.atras_Click);
+            // 
+            // adelante
+            // 
+            this.adelante.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adelante.Location = new System.Drawing.Point(154, 422);
+            this.adelante.Name = "adelante";
+            this.adelante.Size = new System.Drawing.Size(105, 44);
+            this.adelante.TabIndex = 7;
+            this.adelante.Text = ">>";
+            this.adelante.UseVisualStyleBackColor = true;
+            this.adelante.Click += new System.EventHandler(this.adelante_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = global::practica2POE.Properties.Resources.color10;
+            this.pictureBox1.Location = new System.Drawing.Point(43, 230);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 186);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 523);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.adelante);
+            this.Controls.Add(this.atras);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button atras;
+        private System.Windows.Forms.Button adelante;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ practica2POE/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica2POE
+{
+    public partial class Form1 : Form
+    {  
+        //arreglo aca para almacenar las fotos
+        private Image[] images;
+        private int currentIndex = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadImages();//para cargar las imagenes 
+            UpdateImage(); // para mostrar la imagen
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //tomar etiqueta y asignarle un cambio al contenido 
+            //mediante el evento del boton
+
+            label1.Text = "Despues del evento";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Antes del evento";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            //se pone el nombre del evento + el metodo de cambio
+            //modificando el color del panel en aleatorio
+
+            Color randomcolor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            panel1.BackColor = randomcolor;
+        }
+        private void LoadImages()
+        {
+            images = new Image[]
+            {
+                // carga las imágenes desde los recursos y
+                // las almacena en el arreglo
+                Properties.Resources.color1,
+                Properties.Resources.color10,
+                Properties.Resources.color13,
+                Properties.Resources.color2,
+                Properties.Resources.color3,
+                Properties.Resources.color4,
+                Properties.Resources.color5,
+                Properties.Resources.color6,
+                Properties.Resources.color7,
+                Properties.Resources.color8,
+                Properties.Resources.color9,
+
+
+            };
+        }
+        private void UpdateImage()
+        {
+            //mostrara la imagen actual del picture box
+            pictureBox1.Image = images[currentIndex];
+        }
+
+        private void ChangeImage(int delta)
+        {
+            // cambia la foto actual avanzando
+            // o retrocediendo según el valor de 'delta'
+            currentIndex = (currentIndex + delta + images.Length) % images.Length;
+            UpdateImage();
+        }
+
+        private void atras_Click(object sender, EventArgs e)
+        {
+            //cambia la imagen para atras dando 
+            //en el boton 
+            ChangeImage(-1);
+        }
+
+        private void adelante_Click(object sender, EventArgs e)
+        {
+            //y esta para adelante 
+            ChangeImage(1);
+        }
+    }
+}
